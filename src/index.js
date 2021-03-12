b43ce973c3ddbdc011be60d3f06518e751a7d15b
Eugene <tsai.evgenii@gmail.com>
2021-03-12 08:30:50 +0300

    feat: max, min and avg functions have been implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    if (typeof array === 'undefined' || array.length === 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array === 'undefined' || array.length === 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (typeof array === 'undefined' || array.length === 0) {
+        return 0;
+    }
+    const sum = array.reduce((sum, current) => sum + current, 0);
+    return sum / array.length;
+};
